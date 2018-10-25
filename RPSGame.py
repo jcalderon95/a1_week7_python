f22b8c4eaea4781981d8bf0259d40aff83172029
Calderon <j_calderon@tss-hr420-dm22.fc.ca>
2018-10-25 15:05:47 -0400

    added conditions to RPSGame,py

--- RPSGame.py
+++ RPSGame.py
@@ -2,10 +2,16 @@ from random import randint
 
 # available weapons => store in an array
 choices = ["Rock", "Paper", "Scissors"]
-player = false
+player = False
 
 # make the computer pick one item at random
-computer_choice = choices[randint(0,2)]
+computer_choice = choices[randint(0, 2)]
 
 # show the computer's choice in the terminal window
 print("Computer chooses: ", computer_choice)
+
+while player is False:
+	print("Choose your weapon!\n")
+	player = input("Rock, Paper or Scissors? \n")
+	print("Player chooses", player)
+
